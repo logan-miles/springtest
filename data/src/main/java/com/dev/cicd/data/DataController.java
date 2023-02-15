996d970687372a47be985b0a2a2e0d9e48e8a48c
logan <logan.e.miles@gmail.com>
2023-02-15 16:45:40 -0700

    Controllers

--- data/src/main/java/com/dev/cicd/data/DataController.java
+++ data/src/main/java/com/dev/cicd/data/DataController.java
@@ -1,12 +1,63 @@
 package com.dev.cicd.data;
 
+import java.util.Locale;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.github.javafaker.Currency;
+import com.github.javafaker.Faker;
+import com.github.javafaker.Nation;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 @RestController
 public class DataController {
     @GetMapping("/")
     public String healthCheck() {
         return "HEALTH CHECK OK!";
+    }
+
+    @GetMapping("/version")
+    public String version() {
+        return "The actual version is 1.0.0";
+    }
+
+    @GetMapping("/nations")
+    public JsonElement getRandomNations() {
+        Faker faker = new Faker(new Locale("en-US"));
+        
+        JsonArray nations = new JsonArray();
+        for (int i = 0; i < 10; i++) {
+            Nation nation = faker.nation();
+            JsonObject jNation = new JsonObject();
+            jNation.addProperty("nationality", nation.nationality());
+            jNation.addProperty("capitalCity", nation.capitalCity());
+            jNation.addProperty("flag", nation.flag());
+            jNation.addProperty("language", nation.language());
+
+            nations.add(jNation);
+        }
+
+        return nations;
+    }
+
+    @GetMapping("/currencies")
+    public JsonElement getRandomCurrencies() {
+        Faker faker = new Faker(new Locale("en-US"));
+        JsonArray currencies = new JsonArray();
+
+        for (int i = 0; i < 20; i++) {
+            Currency currency = faker.currency();
+
+            JsonObject jCurrency = new JsonObject();
+            jCurrency.addProperty("name", currency.name());
+            jCurrency.addProperty("code", currency.code());
+
+            currencies.add(jCurrency);
+        }
+
+        return currencies;
     }
 }
--- data/src/test/java/com/dev/cicd/data/DataControllerTests.java
+++ data/src/test/java/com/dev/cicd/data/DataControllerTests.java
@@ -1,10 +1,13 @@
 package com.dev.cicd.data;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+
+import com.google.gson.JsonElement;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class DataControllerTests {
@@ -18,5 +21,27 @@ public class DataControllerTests {
     @Test
     public void healthCheckShouldReturnOk() {
         assertEquals("HEALTH CHECK OK!", dc.healthCheck());
+    }
+
+    @Test
+    void testVersionShouldBe1_0_0() {
+        assertEquals("The actual version is 1.0.0", dc.version());
+    }
+
+    @Test
+    void getRandomNationsShouldHaveNationalityCapitalFlagLanguage() {
+        JsonElement nations = dc.getRandomNations();
+
+        assertTrue(nations.isJsonArray());
+        assertEquals(10, nations.getAsJsonArray().size());
+    }
+
+    @Test
+    void testGetRandomCurrencies() {
+        JsonElement currencies = dc.getRandomCurrencies();
+
+        assertTrue(currencies.isJsonArray());
+        assertEquals(20, currencies.getAsJsonArray().size());
+        
     }
 }
